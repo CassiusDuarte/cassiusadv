990b592aade67b350e64c26b090b27577be740ee
CassiusDuarte <cassius.duarte@gmail.com>
2026-04-04 11:46:48 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://cassiusduarte.github.io',
+  base: '/cassiusadv',
   vite: {
     plugins: [tailwindcss()],
   },
