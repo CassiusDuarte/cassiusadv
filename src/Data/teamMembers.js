e8290be17666bfea89105651bb84b3371236a249
Cassius.Duarte <cassius.duarte@gmail.com>
2026-04-07 18:49:33 -0300

    fixing some spelling

--- src/Data/teamMembers.js
+++ src/Data/teamMembers.js
@@ -5,12 +5,12 @@ export const teamMembers = [
     image: "/cassiusadv/CRD.jpeg",
     description:
       "Atuação na área de Direito Médico e Educacional voltado à direito de TEA e PCD, com foco consultivo, negocial e contencioso.",
-  },
+  }/*,
   {
     name: "Jose G. Wayand ",
     role: "Estagiário de Direito Médico e Educacional",
     image: "/cassiusadv/JGW.jpg",
     description:
       "Suporte para Direito Médico, causas envolvendo Planos de Saúde e litígios responsabilidade Civil, especialmente médica.",
-  }
+  }*/
 ];
