587c055113f7cb89ddf26ddc1f286603d005d6ef
Cassius.Duarte <cassius.duarte@gmail.com>
2026-04-07 19:27:25 -0300

    fixing more spelling

--- src/Data/blogData.js
+++ src/Data/blogData.js
@@ -1,7 +1,7 @@
 export const blogPosts = [
   {
     id: 1,
-    title: "Escola nega disponibilização de assistente especializado, o que fazer?",
+    title: "Escola nega disponibilização de assistente especializado. O que fazer?",
     description: "Quais são as alternativas jurídicas disponíveis para o caso em que a Escola se nega .",
     image: "/cassiusadv/TEA1.png",
     slug: "tea-1",
@@ -14,16 +14,16 @@ export const blogPosts = [
       <ol class="list-decimal list-inside">
      <li> Notificação Extrajudicial e posterior judicialização, caso não haja acordo.</li>
       <li> Acionar a delegacia de ensino.</li>
-      <li> Procurar o auxílio da Defensoria Pública</li>
+      <li> Procurar o auxílio da Defensoria Pública.</li>
       <li> Efetuar uma reclamação sobre a conduta da Escola na Ouvidoria do MPSP - Ministério Público de São Paulo, ou o MP de seu Estado.</li>
       </ol>
 
-      <p>Das medidas acima, a mais efetiva é a reclamação da Escola feita pela Ouvidoria do Ministério Público. Conforme "Manual de Atuação do Ministério Público
+      <p>Das medidas acima, a mais efetiva é a reclamação da Escola feita na Ouvidoria do Ministério Público. Conforme "Manual de Atuação do Ministério Público
    em Defesa da Educação Especial na Perspectiva da Educação Inclusiva", publicado pelo CNMP - Conselho Nacional do Ministério Público, o MP deve dar prioridade
-  a atuações coletivas. Entretanto, as atuações para direito individuais podem ter repercussões na esfera dos direitos difusos.</p>
+  a atuações coletivas. Entretanto, atuações voltadas a direito individuais podem gerar repercussões na esfera dos direitos difusos.</p>
 
   <p> Para a maior efetividade de sua reclamação, faça no site do MP <a  class="text-blue-500" href="https://sis.mpsp.mp.br/atendimentocidadao/Promotorias/Consulta/EscolherTipoDeIdentificacaoOuvidoria"> (neste link)</a>
-  Através da sequência "Contate o MPSP" -> Atendimento ao Cidadão -> Ouvidoria -> Nova Manifestação.
+  através da sequência "Contate o MPSP" -> "Atendimento ao Cidadão" -> "Ouvidoria" -> "Nova Manifestação".
 
   É importante que sua reclamação foque nos fatos, seja embasada no laudo médico solicitando o assistente especializado (ou mediador escolar). Não se esqueça
   de anexar e-mails e atas comprovando a cobrança efetuada.
